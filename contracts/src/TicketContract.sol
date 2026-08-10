304a1e92238d27d99b4b7f256c693cb2e11ceb99
Blurryface275 <blurryface@Ronald.localdomain>
2026-08-10 08:45:05 +0700

    init: OpenZeppelin module

---
+++ contracts/src/TicketContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.33;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyERC721 is ERC721, ERC721Burnable, ERC721Pausable, ERC721Upgradeable {    
+    
+    
+}
